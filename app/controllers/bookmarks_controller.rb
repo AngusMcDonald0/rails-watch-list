63f1ac5516e8fa0ce7ffac2862fe09f81aa74a24
angus mcdonald <angusmcdonald13@gmail.com>
2022-09-01 16:48:36 +1000

    push

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,10 +1,12 @@
 class BookmarksController < ApplicationController
   def new
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
     @bookmark.list = List.find(params[:list_id])
     if @bookmark.save
       redirect_to list_path(@bookmark.list)
@@ -22,6 +24,6 @@ class BookmarksController < ApplicationController
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment, :list_id, :movie_id)
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
